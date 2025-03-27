6702b2a0a58fec3541b1dca4ae9adbd5668a144c
Daniil Z. <zemnowf@DESKTOP-EJNGLG6.>
2025-03-27 23:07:27 +0300

    minor sass changes

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -2,8 +2,12 @@
 
 @section('content')
         <div class="wrapper">
-            <p>
+            <p class="wrapper">
                 test place
             </p>
+            <a class="wrapper" href="/">
+                test link
+            </a>
+            <img src="{{ asset('files/saladan-test1.jpg') }}">
         </div>
 @endsection
