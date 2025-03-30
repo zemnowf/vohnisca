59d1211877b597d199ae7a77980b9b41c6b98c7f
Daniil Z. <zemnowf@DESKTOP-EJNGLG6.>
2025-03-30 23:15:47 +0300

    main template

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,13 +1,50 @@
 @extends('layouts.app')
 
 @section('content')
-        <div class="wrapper">
-            <p class="wrapper">
-                test place
+    <div class="main-campaigns container">
+        <div class="main-campaign-info">
+            <h3>Saladan</h3>
+            <p>
+                Blood and Sand
             </p>
-            <a class="wrapper" href="/">
-                test link
-            </a>
-            <img src="{{ asset('files/saladan-test1.jpg') }}">
+            <p>
+                A scorching sun beats down on the desert metropolis of Saladan,
+                where gladiators bleed for gold, rebels whisper in smoke-filled taverns,
+                and ancient secrets lie buried beneath the sands. The Emir rules with an iron fist,
+                the Temple of Flame fans the fires of faith, and shadowy factions vie for power.
+            </p>
+            <p>
+                You were brought here in chains. Now, fate—or a dagger in the dark—will decide
+                if you die in the arena, rise as a legend, or unravel the city’s cursed history.
+
+            </p>
+            <p>
+                Will you fight for freedom, for power… or simply to survive?
+            </p>
+            <img src="/files/saladan.webp" alt="Saladan">
         </div>
+        <div class="main-campaign-info">
+            <h3>Browleime</h3>
+            <p>
+                City of Salt, Secrets, and Storm
+            </p>
+            <p>
+                Nestled between the crashing waves and the whispering pines, the port city of Browleim
+                thrives on trade, faith, and buried legends. Its cobbled streets echo with the clamor of
+                merchants, the prayers of zealots, and the hushed deals of those who sail beyond the horizon.
+            </p>
+            <p>
+                But the tides bring more than ships—they carry rumors of a forgotten island, where a
+                pirate’s hoard and an ancient power lie waiting. Some seek fortune, others redemption,
+                and a few dare to chase the flower that burns without ash, the temple drowned beneath the
+                waves, or the storm that never sleeps.
+
+            </p>
+            <p>
+                Will you answer the call of the sea? Or will Browleime’s secrets drag you under?
+            </p>
+            <img src="/files/browleime.webp" alt="Browleime">
+        </div>
+
+    </div>
 @endsection
